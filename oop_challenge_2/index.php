0ba26300bf4589a55870d21e3b20547ee6eb79f0
object-ions <moshikoatia@gmail.com>
2024-01-18 16:43:17 -0800

    add challenges

---
+++ oop_challenge_2/index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+## Challenge 2
+
+1. Create a class called `StringUtility` that has the following static methods:
+
+- `shout($string)` - Takes in a string and returns it in all uppercase letters with an exclamation mark added to the end.
+
+- `whisper($string)` - Takes in a string and returns it in all lowercase letters with a period added to the end.
+
+- `repeat($string, $times)` - Takes in a string and a number and returns the string repeated the specified number of times. Use a default value of `2` for the `$times` parameter.
+
+2. Create a new instance of the `StringUtility` class and call each of the methods on it.
+
+#### Hints
+
+- You can use the `strtoupper` and `strtolower` functions to convert a string to uppercase or lowercase.
+- You can use the `str_repeat` function to repeat a string a certain number of times.
+*/
