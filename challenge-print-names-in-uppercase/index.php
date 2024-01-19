c97c8ab019e675d5f5ff75938e91182326654a51
object-ions <moshikoatia@gmail.com>
2024-01-18 17:46:48 -0800

    add solution

--- challenge-print-names-in-uppercase/index.php
+++ challenge-print-names-in-uppercase/index.php
@@ -3,3 +3,14 @@
   Challenge 2: Print names in uppercase
   Create a function called `printNamesToUpperCase` that takes an array of names as an argument. The function should loop through the array and print each name to the screen in uppercase letters.
 */
+
+$arrNames = ['Moses', 'Jon', 'Bob', 'Cameron'];
+
+function printNamesToUpperCase ($arrOfNames) {
+  foreach ($arrOfNames as $name) {
+    $newName = strtoupper($name);
+    echo $newName . '<br>';
+  }
+}
+
+printNamesToUpperCase($arrNames);
